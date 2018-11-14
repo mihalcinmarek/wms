7ab2f9178650f3b21079c6858ed9685280048166
marek.mihalcin <mihalcin.marek@gmail.com>
2018-11-14 21:29:23 +0100

    uz to fachci

--- server.js
+++ server.js
@@ -1,28 +1,27 @@
-
-var express = require('express');
-var path = require ('path');
+var express = require("express");
 var server = express();
-var fs = require ('fs');
-var mapnik = require ('mapnik')
-var generateImage = require ('./generate_img.js');
-var PORT = 3456;
+var path=require('path');
+var fs = require("fs");
+var mapnik = require("mapnik");
+var generateImage = require('./generateimage.js');
+var PORT=3456;
 
-
-console.log(generateImage);
-
-server.get('/wms', function (request, response) {
-    var params = request.query;
+server.get("/wms", function(request, response) {
+  var params=request.query;
     console.log(params);
-
-    if (params.service === 'WMS' && params.request === 'GetCapabilities') {
-        response.sendFile (path.join(__dirname , 'GetCapabilities.xml'))
-    } else if (params.service === 'WMS' && params.request === 'getMap'){
-        generateImage(params, response.sendFile.bind(response))
+   if (params.SERVICE=== 'WMS' && params.REQUEST === 'GetCapabilities' ){            
+       response.sendFile(path.join(__dirname, 'xmlko.xml'))
+	 }else if (params.SERVICE==="WMS"&& params.REQUEST==="GetMap"){
+     generateImage(params, response.sendFile.bind(response))	  
     } else {
-        response.send ('posral si to')
+      response.send('posral si to')
     }  
   });
 
 server.listen(PORT, function() {
-    console.log("Server listening on port " + PORT + "!")
-});
+    console.log("Server listening on port " + PORT + "!");
+  });
+
+  console.log(generateImage);
+  
+ 
--- wtgis-examples/importing_variables/importing_variables.js
+++
@@ -1,5 +0,0 @@
-var path = require("path");
-var myVariables = require(path.join(__dirname, "/variables.js"));
-// now we have access to stuff exported from variables.js
-console.log(myVariables);
-myVariables.someFunction(3)
